3e31eb79821f0ab4ac1f955fc0518537694cb941
Pretty Pandey <prettypandeypearl@gmail.com>
2023-09-02 22:15:32 +0200

    Task 4

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@
  */
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
-import UsersController from '../controllers/UsersController';
+import UserController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import FilesController from '../controllers/FilesController';
 
@@ -11,7 +11,7 @@ const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStatus);
-router.post('/users', UsersController.postNew);
+router.post('/users', UserController.postNew);
 router.get('/users/me', UserController.getMe);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
