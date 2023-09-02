d989ef99d911edfc6256765cea3fded76a505b45
Pretty Pandey <prettypandeypearl@gmail.com>
2023-09-02 17:31:02 +0200

    Task 0 eslint compliance

--- utils/redis.js
+++ utils/redis.js
@@ -5,33 +5,33 @@ import { promisify } from 'util';
 import { createClient } from 'redis';
 
 class RedisClient {
-    constructor() {
-        this._client = createClient();
-        this.connected = true;
-        this._client.on('error', (err) => {
-            console.log('Redis connector error:', err.message || err.toString());
-            this.connected = false;
-        });
-    }
+  constructor() {
+    this._client = createClient();
+    this.connected = true;
+    this._client.on('error', (err) => {
+      console.log('Redis connector error:', err.message || err.toString());
+      this.connected = false;
+    });
+  }
 
-    isAlive() {
-        return this.connected;
-    }
+  isAlive() {
+    return this.connected;
+  }
 
-    async get(key) {
-        const GET = promisify(this._client.GET);
-        return GET.call(this._client, key);
-    }
+  async get(key) {
+    const GET = promisify(this._client.GET);
+    return GET.call(this._client, key);
+  }
 
-    async set(key, value, expires) {
-        const SETEX = promisify(this._client.SETEX);
-        await SETEX.call(this._client, key, expires, value);
-    }
+  async set(key, value, expires) {
+    const SETEX = promisify(this._client.SETEX);
+    await SETEX.call(this._client, key, expires, value);
+  }
 
-    async del(key) {
-        const DEL = promisify(this._client.DEL);
-        return DEL.call(this._client, key);
-    }
+  async del(key) {
+    const DEL = promisify(this._client.DEL);
+    return DEL.call(this._client, key);
+  }
 }
 
 const redisClient = new RedisClient();
